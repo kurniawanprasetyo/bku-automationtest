ff0543c42dd2e3bd92c4637ee1fb27c7b8eebc09
Elang <elangbayu17@gmail.com>
2021-08-05 09:55:06 +0700

    Datepicker temp solution

--- cypress/integration/Routing.spec.js
+++ cypress/integration/Routing.spec.js
@@ -1,10 +1,9 @@
 describe('Routing', function(){
     beforeEach(function(){
         cy.visit('/auth/login')
-        cy.fixture('Authentication')
-        .then(testdata => {
-            this.testdata = testdata;
-        })
+        //Login as admin
+        cy.get('input[name=user]').type(Cypress.env('username_admin'));
+        cy.get('input[name=password]').type(Cypress.env('password_admin'));
         cy.fixture('CollectionAgent')
         .then(userdata =>{
             this.userdata = userdata;
@@ -12,9 +11,6 @@ describe('Routing', function(){
     })
 
     it('Create Routing', function(){
-        //Login as admin
-        cy.get('input[name=user]').type(this.testdata.admin_username);
-        cy.get('input[name=password]').type(this.testdata.admin_password);
         cy.contains('Sign In').click();
         //Go to virtual account menu
         cy.contains('Routing and Destination').click();
@@ -27,8 +23,24 @@ describe('Routing', function(){
         cy.get('select[name=destination_code]').select(this.userdata.destination_code);
         cy.get('select[name=transaction_type]').select('C2C');
         //Still Confusing Handle datepicker
-        cy.get('[name="start_date"]').click();
-        cy.datepicker('28-November-2021');
+        // cy.get('[name="start_date"]').click();
+        cy.get('input[name="start_date"]').invoke('val').then((text) => {
+            expect('2021-08-06').to.equal(text);
+        });
+        cy.get('input[name="end_date"]').clear();
+        for(let n = 0; n < 12; n ++){
+            cy.get('button.react-datepicker__navigation.react-datepicker__navigation--next').click()
+        }
+        cy.get('div.react-datepicker__day.react-datepicker__day--006').first().click()
+        cy.get('input[name="end_date"]').invoke('val').then((text) => {
+            expect('2022-08-06').to.equal(text);
+        });
+        // cy.get('input[name="end_date"]').clear().click().type('2022-08-05', {force: true}).trigger('change');
+        // cy.get('input[name="end_date"]').invoke('val', '2022-08-05').trigger('change').then((text) => {
+        //     // expect('2022-08-05').to.equal(text);
+        //     console.log(text);
+        // });
+        // cy.datepicker('28-November-2021');
         // cy.get('select[name=mitra_id]').select(this.userdata.mitra_id);
         // cy.get('select[name=flag_auto_route]').select('INACTIVE');
         // //Klik cancel for temporary
