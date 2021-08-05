be84078edce29848969cc94f836a71339681d5a3
kurniawan <kurniawan.prasetyo@artajasa.co.id>
2021-08-05 15:58:40 +0700

    Impelement Login Command

--- cypress/integration/TransactionConfig.spec.js
+++ cypress/integration/TransactionConfig.spec.js
@@ -1,9 +1,8 @@
 describe('Transaction Config', function(){
     beforeEach(function(){
-        cy.visit('/auth/login')
         cy.fixture('Authentication')
-        .then(testdata => {
-            this.testdata = testdata;
+        .then(logindata =>{
+            this.logindata = logindata;
         })
         cy.fixture('CollectionAgent')
         .then(userdata =>{
@@ -12,19 +11,17 @@ describe('Transaction Config', function(){
     })
     it('Create Transaction Config', function(){
         //Login as admin
-        cy.get('input[name=user]').type(this.testdata.admin_username);
-        cy.get('input[name=password]').type(this.testdata.admin_password);
-        cy.contains('Sign In').click();
+        cy.login(this.logindata.admin_username, this.logindata.admin_password);
         //Go to transaction config menu
         cy.contains('Transaction Config').click();
         cy.contains('New Account Transaction Config').click();
         //Fill Form
         cy.get('select[name=ca_id]').select(this.userdata.institution_id);
-        cy.get('input[name=username]').type(this.userdata.username);
+        cy.get('input[name=username]').type(this.logindata.username);
         cy.get('input[name=token_time]').type('10');
         cy.get('select[name=token_duration]').select('minutes');
-        cy.get('input[name=password]').first().type(this.userdata.password);
-        cy.get('input[name=confirm_password]').first().type(this.userdata.password)
+        cy.get('input[name=password]').first().type(this.logindata.password);
+        cy.get('input[name=confirm_password]').first().type(this.logindata.password)
         cy.contains('Submit').click();
         //Log Out
         cy.get('.btn > .symbol > .symbol-label').click();
