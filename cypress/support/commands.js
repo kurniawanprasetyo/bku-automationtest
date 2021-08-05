89c0c81d251e9e397f67ea0c09e200203c3f4233
kurniawan <kurniawan.prasetyo@artajasa.co.id>
2021-08-05 16:00:04 +0700

    Add Login Command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,36 +7,44 @@ Cypress.Commands.add('generate_random_number', (length) => {
     return random_number
    });
 
-Cypress.Commands.add('datepicker', (date) =>{
-    var day = date.split('-')[0];
-    console.log(day);
-    var month = date.split('-')[1];
-    console.log(month);
-    var year = date.split('-')[2];
-    console.log(year);
+// Cypress.Commands.add('datepicker', (date) =>{
+//     var day = date.split('-')[0];
+//     console.log(day);
+//     var month = date.split('-')[1];
+//     console.log(month);
+//     var year = date.split('-')[2];
+//     console.log(year);
     
-    var chooseMonth = ''
-    var chooseYear = ''
+//     var chooseMonth = ''
+//     var chooseYear = ''
 
-    cy.get('.react-datepicker__current-month').invoke('text')
-        .then((yearMonth) => {
-            var chooseMonthYear = yearMonth;
-            chooseMonth = chooseMonthYear.split(' ')[0];
-            cy.log(chooseMonth);
-            chooseYear = chooseMonthYear.split(' ')[1];
-            cy.log(chooseYear);
-        })
+//     cy.get('.react-datepicker__current-month').invoke('text')
+//         .then((yearMonth) => {
+//             var chooseMonthYear = yearMonth;
+//             chooseMonth = chooseMonthYear.split(' ')[0];
+//             cy.log(chooseMonth);
+//             chooseYear = chooseMonthYear.split(' ')[1];
+//             cy.log(chooseYear);
+//         })
         
-            while (chooseMonth !== month || chooseYear !== year){
-                console.log("OKE");
-                cy.get('.react-datepicker__navigation--next').click();
-                cy.get('.react-datepicker__current-month').invoke('text')
-                    .then((yearMonth) => {
-                        chooseMonth = yearMonth.split(' ')[0];
-                        console.log(chooseMonth)
-                        chooseYear = yearMonth.split(' ')[1];
-                        console.log(chooseYear)
-                    })
-                break;
-                }
+//             while (chooseMonth !== month || chooseYear !== year){
+//                 console.log("OKE");
+//                 cy.get('.react-datepicker__navigation--next').click();
+//                 cy.get('.react-datepicker__current-month').invoke('text')
+//                     .then((yearMonth) => {
+//                         chooseMonth = yearMonth.split(' ')[0];
+//                         console.log(chooseMonth)
+//                         chooseYear = yearMonth.split(' ')[1];
+//                         console.log(chooseYear)
+//                     })
+//                 break;
+//                 }
+// })
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.visit('/auth/login')
+        //Login as admin
+    cy.get('input[name=user]').type(username);
+    cy.get('input[name=password]').type(password);
+    cy.contains('Sign In').click();
 })
